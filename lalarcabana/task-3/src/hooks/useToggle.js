eb16cfda7af8468d2f3bbd90d4be9b4bf1374c69
duudu <dolgoongantumur78@gmail.com>
2026-05-19 23:24:15 +0800

    Delete lalarcabana directory

--- lalarcabana/task-3/src/hooks/useToggle.js
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export default function useToggle(initialValue = false) {
-  const [value, setValue] = useState(Boolean(initialValue));
-
-  const toggle = () => setValue((prev) => !prev);
-
-  return { value, toggle, setValue };
-}
